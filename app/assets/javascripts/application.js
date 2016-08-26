13f282ebe213d9e0a78ab837bd7597147d4ba66c
Pedro Alvarado <pedro.alvarado@globant.com>
2016-08-26 07:05:30 -0500

    Require adminlte in stylesheeta and javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,12 +5,25 @@
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
+// compiled file.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
+//= require autonumeric
+//= require moment
+//= require moment/es
+//= require bootstrap-sprockets
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
+//= require bootstrap-datetimepicker
+//= require select2
+//= require select2_locale_es
+//= require fullcalendar
+//= require fullcalendar/lang-all
 //= require turbolinks
+//= require adminlte
 //= require_tree .
