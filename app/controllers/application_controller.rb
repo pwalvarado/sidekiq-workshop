c7843e4ebccbd724ff75010e15f2a68207239257
Pedro Alvarado <pedro.alvarado@globant.com>
2016-08-28 15:08:08 -0500

    add devise layout close #21

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+  protected :layout_by_resource
 end
